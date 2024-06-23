59d3999118859e2fc7e492f2e06555729f223fef
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-06-23 15:06:50 -0400

    feat: add dtos

---
+++ DTOs/ErrorMessageDto.cs
@@ -0,0 +1,6 @@
+namespace hackadisc_dotnet_api.DTOs;
+
+public class ErrorMessageDto
+{
+    public required string Error { get; set; }
+}
---
+++ DTOs/UpdateMemberDto.cs
@@ -0,0 +1,41 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace hackadisc_dotnet_api.DTOs;
+
+/// <summary>
+/// DTO para la actualización de un integrante.
+/// </summary>
+public class UpdateMemberDto
+{
+    /// <summary>
+    /// Nombre del integrante.
+    /// </summary>
+    [Required(ErrorMessage = "El nombre es requerido.")]
+    [StringLength(
+        20,
+        MinimumLength = 3,
+        ErrorMessage = "El nombre debe tener entre 3 y 20 caracteres."
+    )]
+    public required string Name { get; set; }
+
+    /// <summary>
+    /// Correo electrónico del integrante.
+    /// </summary>
+    [Required(ErrorMessage = "El correo electrónico es requerido.")]
+    [EmailAddress(ErrorMessage = "Correo electrónico inválido.")]
+    public required string Email { get; set; }
+
+    /// <summary>
+    /// Número de semestre del integrante.
+    /// </summary>
+    [Required(ErrorMessage = "El semestre es requerido.")]
+    [Range(1, 10, ErrorMessage = "El semestre debe estar entre 1 y 10.")]
+    public required int Semester { get; set; }
+
+    /// <summary>
+    /// Carrera del integrante.
+    /// </summary>
+    [Required(ErrorMessage = "La carrera es requerida.")]
+    [EnumDataType(typeof(Career), ErrorMessage = "Carrera inválida.")]
+    public required string Career { get; set; }
+}
