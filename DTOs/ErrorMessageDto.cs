ed9d2ed39e54d87fdb5c03cf395f774c738ce34f
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-06-23 15:19:16 -0400

    feat: add error message dto

--- DTOs/ErrorMessageDto.cs
+++ DTOs/ErrorMessageDto.cs
@@ -1,6 +1,12 @@
 namespace hackadisc_dotnet_api.DTOs;
 
+/// <summary>
+/// DTO para mensajes de error.
+/// </summary>
 public class ErrorMessageDto
 {
+    /// <summary>
+    /// Mensaje de error.
+    /// </summary>
     public required string Error { get; set; }
 }
